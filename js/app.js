d67999b724638d2c00a600d7d4b7cd8f95f5552e
diku952 <dimaku666@gmail.com>
2020-11-13 20:46:06 +0300

    правки js

--- js/app.js
+++ js/app.js
@@ -36,41 +36,6 @@ function funk() {
 }
 
 // *Действия в меню
-document.addEventListener('DOMContentLoaded', function () {
-    let form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
-
-    async function formSend(e) {
-      e.preventDefault();
-
-      let error = formValidate(form);
-    }
-      
-      function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-
-        for (let index = 0; index < formReq.length; index++) {
-          const input = formReq[index];
-          formRemoveError(input);
-
-          if (input.value === '') {
-            formAddError(input);
-            error++;
-          }
-
-        }
-    }
-    function formAddError(input) {
-      input.parentElement.classList.add('_error');
-      input.classList.add('_error');
-    }
-    function formRemoveError(input) {
-      input.parentElement.classList.remove('_error');
-      input.classList.remove('_error');
-    }
-});
-
 $('#form').submit(function (e) {
   e.preventDefault();
   $.ajax({
@@ -85,6 +50,41 @@ $('#form').submit(function (e) {
     }
   });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
+
+//     async function formSend(e) {
+//       e.preventDefault();
+
+//       let error = formValidate(form);
+//     }
+      
+//       function formValidate(form) {
+//         let error = 0;
+//         let formReq = document.querySelectorAll('._req');
+
+//         for (let index = 0; index < formReq.length; index++) {
+//           const input = formReq[index];
+//           formRemoveError(input);
+
+//           if (input.value === '') {
+//             formAddError(input);
+//             error++;
+//           }
+
+//         }
+//     }
+//     function formAddError(input) {
+//       input.parentElement.classList.add('_error');
+//       input.classList.add('_error');
+//     }
+//     function formRemoveError(input) {
+//       input.parentElement.classList.remove('_error');
+//       input.classList.remove('_error');
+//     }
+// });
 
 // !Liquid buttton scroll to packages
 var hiddenElement = document.getElementById("packages");
